8a1284cbb45a9cc061a7333c127033afcaee36d9
Михайло <153002746+mishajr@users.noreply.github.com>
2024-02-29 22:07:57 +0200

    Add files via upload
    Commit home work4

--- HomeWork4/HomeWork4.cpp
+++ HomeWork4/HomeWork4.cpp
@@ -1,7 +1,102 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
-int main()
-{
-   cout << "Hello World!\n";
+
+int main() {
+
+
+    //Завдання 1
+    // 
+    //int number;
+
+    //cout << "Enter a six-digit number: ";
+    //cin >> number;
+
+
+    //if (number < 100000 || number > 999999) {
+    //    cout << "Error: Please enter a six-digit number." << endl;
+    //   
+    //}
+
+    //int firstThreeDigitsSum = number / 100000 + (number / 10000) % 10 + (number / 1000) % 10;
+    //int lastThreeDigitsSum = (number / 100) % 10 + (number / 10) % 10 + number % 10;
+
+    //if (firstThreeDigitsSum == lastThreeDigitsSum) {
+    //    cout << "Congratulations! You have a lucky number." << endl;
+    //}
+    //else {
+    //    cout << "Sorry! Your number is not lucky." << endl;
+    //}
+
+    //Завдання 2
+  /*  int number;
+
+    cout << "Enter a four-digit number: ";
+    cin >> number;
+
+    if (number < 1000 || number > 9999) {
+        cout << "Error: Please enter a four-digit number." << endl;
+        return 1;
+    }
+
+    int newNumber = (number % 100) * 100 + (number / 100);
+
+    cout << "The modified number is: " << newNumber << endl;*/
+
+
+    //Завдання 3
+    //int a, b, c, d, e, f, g;
+
+    //cout << "Enter 7 integers: " << endl;
+    //cin >> a >> b >> c >> d >> e >> f >> g;
+
+    //int maxNumber = a;
+    //if (b > maxNumber) maxNumber = b;
+    //if (c > maxNumber) maxNumber = c;
+    //if (d > maxNumber) maxNumber = d;
+    //if (e > maxNumber) maxNumber = e;
+    //if (f > maxNumber) maxNumber = f;
+    //if (g > maxNumber) maxNumber = g;
+
+    //cout << "The maximum number is: " << maxNumber << endl;
+
+    //Завдання 4
+    //int distanceAB, distanceBC, weight;
+    //float fuelConsumptionAB, fuelConsumptionBC, fuelNeeded;
+
+    //cout << "Enter the distance between points A and B in km: ";
+    //cin >> distanceAB;
+
+    //cout << "Enter the distance between points B and C in km: ";
+    //cin >> distanceBC;
+
+    //cout << "Enter the weight of the cargo in kg: ";
+    //cin >> weight;
+
+    //if (weight <= 500)
+    //    fuelConsumptionAB = 1;
+    //else if (weight <= 1000)
+    //    fuelConsumptionAB = 4;
+    //else if (weight <= 1500)
+    //    fuelConsumptionAB = 7;
+    //else if (weight <= 2000)
+    //    fuelConsumptionAB = 9;
+    //else {
+    //    cout << "The aircraft cannot lift cargo exceeding 2000 kg." << endl;
+    //}
+
+    //fuelConsumptionBC = fuelConsumptionAB;
+
+    //fuelNeeded = distanceAB * fuelConsumptionAB + distanceBC * fuelConsumptionBC;
+
+    //if (fuelNeeded <= 300)
+    //    cout << "The minimum amount of fuel needed for refueling at point B to fly from point A to point C is: " << fuelNeeded << " liters." << endl;
+    //else
+    //    cout << "The aircraft cannot fly the required distance due to fuel capacity." << endl;
+
+
+    //Завдання 5
+        //ya ne zrozumiv yak tse zrobit
+
+    //Завдання 6
+        //tut tej same 
 }
