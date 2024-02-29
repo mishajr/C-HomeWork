0987c0f034a653d91cdf727461fb66d05c91e90e
misha <grut5391@gmail.com>
2024-02-29 20:54:35 +0200

    Commit

---
+++ Home Work3/Home Work3.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    //Завдання 1
+
+    //float distance;
+    //float time;
+
+
+    //cout << "Enter the distance to the airport (in kilometers): ";
+    // 
+    //cin >> distance;
+
+    //cout << "Enter the time it takes to travel (in hours): ";
+    // 
+    //cin >> time;
+
+
+    //float speed = distance / time;
+
+
+    //cout << "You need to travel at a speed of " << speed << " km/h." << endl;
+
+    //Завдання 2
+
+    //const double cost_per_minute = 2.0;
+
+    //int start_hours, start_minutes, start_seconds;
+
+    //cout << "Enter the start time of scooter usage (hours minutes seconds): ";
+
+    //cin >> start_hours >> start_minutes >> start_seconds;
+
+    //int end_hours, end_minutes, end_seconds;
+
+    //cout << "Enter the end time of scooter usage (hours minutes seconds): ";
+
+    //cin >> end_hours >> end_minutes >> end_seconds;
+
+    //int start_total_minutes = start_hours * 60 + start_minutes + start_seconds / 60.0;
+
+    //int end_total_minutes = end_hours * 60 + end_minutes + end_seconds / 60.0;
+
+    //int travel_duration = end_total_minutes - start_total_minutes;
+
+    //double travel_cost = travel_duration * cost_per_minute;
+
+    //cout << "The cost of the trip is: " << travel_cost << " UAH." << endl;
+
+
+
+
+    //Завдання 3
+
+   /* double distance, fuelConsumption, priceA, priceB, priceC;
+
+    
+    cout << "Enter the distance (km): ";
+
+    cin >> distance;
+
+    cout << "Enter the fuel consumption per 100 km (liters): ";
+
+    cin >> fuelConsumption;
+
+    cout << "Enter the price of fuel A: ";
+
+    cin >> priceA;
+
+    cout << "Enter the price of fuel B: ";
+
+    cin >> priceB;
+
+    cout << "Enter the price of fuel C: ";
+
+    cin >> priceC;
+
+    double costA = (distance / 100) * fuelConsumption * priceA;
+
+    double costB = (distance / 100) * fuelConsumption * priceB;
+
+    double costC = (distance / 100) * fuelConsumption * priceC;
+
+
+    cout << "Fuel Type\tCost of Travel" << endl;
+
+    cout << "A\t\t" << costA << " $" << endl;
+
+    cout << "B\t\t" << costB << " $" << endl;
+
+    cout << "C\t\t" << costC << " $" << endl;*/
+
+
+
+
+    //Завдання 4
+
+        //int seconds_input;
+
+        //cout << "Enter the time passed since midnight in seconds: ";
+
+        //cin >> seconds_input;
+
+        //int hours = seconds_input / 3600;
+
+        //int minutes = (seconds_input % 3600) / 60;
+
+        //int seconds = (seconds_input % 3600) % 60;
+
+        //cout << "Current time: " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds." << endl;
+
+        //int remaining_seconds = 86400 - seconds_input;
+
+        //int remaining_hours = remaining_seconds / 3600;
+
+        //int remaining_minutes = (remaining_seconds % 3600) / 60;
+
+        //int remaining_seconds_output = (remaining_seconds % 3600) % 60;
+
+        //cout << "Time remaining until midnight: " << remaining_hours << " hours, " << remaining_minutes << " minutes, " << remaining_seconds_output << " seconds." << endl;
+
+
+    //Завдання 5
+
+
+     /* int seconds_input;
+        cout << "Enter the time passed since the beginning of the workday in seconds: ";
+        cin >> seconds_input;
+
+        int remaining_seconds = 28800 - seconds_input;
+        int remaining_hours = remaining_seconds / 3600;
+
+        cout << "Remaining hours at work: " << remaining_hours << " hours." << endl;*/
+}
---
+++ HomeWork2/HomeWork2.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    //Завдання 1
+
+    /*double R1, R2, R3;
+    cout << "Enter the values of resistors R1, R2, and R3: ";
+    cin >> R1 >> R2 >> R3;
+
+    double R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
+
+    cout << "Equivalent resistance R0: " << R0 << endl;*/
+
+
+    //Завдання 2
+    // 
+    //double circumference;
+    //const double pi = 3.14;
+
+    //cout << "Enter the circumference of the circle: ";
+    //cin >> circumference;
+
+    //double radius = circumference / (2 * pi);
+    //double area = pi * radius * radius;
+
+    //cout << "Radius of the circle: " << radius << endl;
+    //cout << "Area of the circle: " << area << endl;
+
+
+    //Завданя 3
+    // 
+    //double v, t, a;
+
+    //cout << "Enter the velocity (v): ";
+    //cin >> v;
+
+    //cout << "Enter the time (t): ";
+    //cin >> t;
+
+    //cout << "Enter the acceleration (a): ";
+    //cin >> a;
+
+    //double distance = v * t + (a * t * t) / 2;
+
+    //cout << "Distance travelled: " << distance << endl;
+
+    //Завданя 4
+
+   /* int seconds, hours, minutes, remainingSeconds;
+
+    cout << "Enter the number of seconds: ";
+
+    cin >> seconds;
+
+    hours = seconds / 3600;
+
+    remainingSeconds = seconds % 3600;
+
+    minutes = remainingSeconds / 60;
+
+    remainingSeconds = remainingSeconds % 60;
+
+    cout << "Hours: " << hours << endl;
+
+    cout << "Minutes: " << minutes << endl;
+
+    cout << "Seconds: " << remainingSeconds << endl;*/
+
+
+
+
+    //Завданя 5
+
+ /*   double amount;
+
+    cout << "Enter the decimal number: ";
+
+    cin >> amount;
+
+    int hryvnia = int(amount);
+
+    int kopeck = int((amount - hryvnia) * 100);
+
+    cout << hryvnia << " UAH " << kopeck << " kop." << endl;*/
+
+
+    //Завданя 6
+    // 
+    //double distanceMeters, timeMinutes, timeSeconds, totalTimeSeconds, speedKmph;
+
+    //cout << "Enter the distance (meters): ";
+    //cin >> distanceMeters;
+
+    //cout << "Enter the time (minutes seconds): ";
+    //cin >> timeMinutes >> timeSeconds;
+
+    //totalTimeSeconds = timeMinutes * 60 + timeSeconds;
+
+    //speedKmph = distanceMeters / 1000 / (totalTimeSeconds / 3600);
+
+    //cout << "Distance: " << distanceMeters << " m" << endl;
+    //cout << "Time: " << timeMinutes << " min " << timeSeconds << " sec = " << totalTimeSeconds << " sec" << endl;
+    //cout << "You ran at a speed of " << speedKmph << " km/h." << endl;
+
+
+    //Завданя 7
+
+   /* int days, weeks, remainingDays;
+
+    cout << "Enter the number of days: ";
+    cin >> days;
+
+    weeks = days / 7;
+    remainingDays = days % 7;
+
+    cout << "Weeks: " << weeks << " and days: " << remainingDays << endl;*/
+
+
+}
