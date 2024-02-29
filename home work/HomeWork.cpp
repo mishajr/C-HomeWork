7f02fab483fbea00f9dfb243da7826deb25b905e
Михайло <153002746+mishajr@users.noreply.github.com>
2024-02-29 21:08:57 +0200

    Add files via upload
    Add homework1

---
+++ home work/HomeWork.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    // 1 Завдання
+   
+  /*  cout << "Step 1: The user inserts the bank card into the ATM" << endl;
+    cout << "Step 2: The ATM asks for a PIN" << endl;
+    
+   
+
+    int userPIN;
+
+    
+
+
+    cout << "Enter your PIN: " << endl;
+    cin >> userPIN;
+
+    cout << "Step 3: Verify your PIN" << endl;
+    int correctPIN = 1234;
+
+    
+ 
+
+    if (userPIN == correctPIN) {
+        cout << "Step 4: Access the main menu" << endl;
+        cout << "The PIN code is correct. You have access to the main menu." << endl;
+      
+    }
+    else {
+        cout << "The PIN code is incorrect. Please try again." << endl;
+    }*/
+
+    //2 завдання 
+
+
+    //cout << "Step 1: The user comes to the library and enters the premises" << endl;
+
+    //cout << "Step 2: User registration" << endl;
+    //string userID;
+    //cout << "Enter your identification number: " << endl;
+    //cin >> userID;
+    //cout << "Book search" << endl;
+    //cout << "Step 4: Book borrowing" << endl;
+    //cout << "Step 5: Book return" << endl;
+    //cout << " Step 6: Exiting the library" << endl;
+ 
+
+
+    //3 завдання 
+
+    //cout << "I wandered lonely as a cloud, \nThat floats on high o'er vales and hills,\nWhen all at once I saw a crowd,\nA host, of golden daffodils;\nBeside the lake, beneath the trees,\nFluttering and dancing in the breeze.\nFluttering and dancing in the breeze.\n- William Wordsworth" << endl;
+        
+    //Завдання 4
+   /* cout << "Every hunter wants to know\nWhere an elephant may go.\nMany enter eastern lands\nJust to try and take a stand.Otherwise, they'll never see\nHimalayan ivory.\n" << endl;*/
+               
+    //Завдяння 5
+   /* cout << "For Sale : \nItem: Vintage bicycle\nCondition: Good\nPrice: $200\nContact: 555-555-5555" << endl;*/
+        
+       
+   
+}
