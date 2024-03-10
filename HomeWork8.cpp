01569c0c8f9f343c79fe59cf6565fa7ef13d444f
Михайло <153002746+mishajr@users.noreply.github.com>
2024-03-10 20:58:48 +0200

    Add files via upload
    add home work 8

---
+++ HomeWork8.cpp
@@ -0,0 +1,154 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    //int num;
+    //cout << "Enter number: ";
+    //cin >> num;
+
+    //const int rows = 4;
+    //const int cols = 3;
+
+    //int arr[rows][cols];
+
+    //for (int i = 0; i < rows; ++i) {
+    //    for (int j = 0; j < cols; ++j) {
+    //        if (j == 0) {
+    //            if (i == 0)
+    //                arr[i][j] = num;
+    //            else
+    //                arr[i][j] = arr[i - 1][cols - 1] * 2;
+    //        }
+    //        else {
+    //            arr[i][j] = arr[i][j - 1] * 2;
+    //        }
+    //    }
+    //}
+    //for (int i = 0; i < rows; ++i) {
+    //    for (int j = 0; j < cols; ++j) {
+    //        cout << arr[i][j] << " ";
+    //    }
+    //    cout << endl;
+    //}
+
+
+
+
+
+
+    //===========================================================================================
+
+    //int num;
+    //cout << "Enter number: ";
+    //cin >> num;
+
+    //const int rows = 4;
+    //const int cols = 3;
+
+    //int arr[rows][cols];
+
+    //for (int i = 0; i < rows; ++i) {
+    //    for (int j = 0; j < cols; ++j) {
+    //        if (j == 0) {
+    //            if (i == 0)
+    //                arr[i][j] = num;
+    //            else
+    //                arr[i][j] = arr[i - 1][cols - 1] + 1;
+    //        }
+    //        else {
+    //            arr[i][j] = arr[i][j - 1] + 1;
+    //        }
+    //    }
+    //}
+    //for (int i = 0; i < rows; ++i) {
+    //    for (int j = 0; j < cols; ++j) {
+    //        cout << arr[i][j] << " ";
+    //    }
+    //    cout << endl;
+    //}
+
+
+
+    //===========================================================================================
+
+    //const int rows = 3;
+    //const int cols = 6;
+    //int arr[rows][cols] = {
+    //    {1, 2, 0, 4, 5, 3},
+    //    {4, 5, 3, 9, 0, 1},
+    //    {2, 4, 9, 6, 1, 7}
+    //};
+
+    //cout << "Initial array:\n";
+    //for (int i = 0; i < rows; ++i) {
+    //    for (int j = 0; j < cols; ++j) {
+    //        cout << arr[i][j] << " ";
+    //    }
+    //    cout << endl;
+    //}
+
+    //int shifts;
+    //cout << "Enter the number of shifts: ";
+    //cin >> shifts;
+
+    //string direction;
+    //cout << "Enter the direction of shift (l == left, r == right, u == up, d == down): ";
+    //cin >> direction;
+
+    //for (int s = 0; s < shifts; ++s) {
+    //    switch (direction[0]) {
+    //    case 'l':
+    //        for (int i = 0; i < rows; ++i) {
+    //            int temp = arr[i][0];
+    //            for (int j = 0; j < cols - 1; ++j) {
+    //                arr[i][j] = arr[i][j + 1];
+    //            }
+    //            arr[i][cols - 1] = temp;
+    //        }
+    //        break;
+    //    case 'r':
+    //        for (int i = 0; i < rows; ++i) {
+    //            int temp = arr[i][cols - 1];
+    //            for (int j = cols - 1; j > 0; --j) {
+    //                arr[i][j] = arr[i][j - 1];
+    //            }
+    //            arr[i][0] = temp;
+    //        }
+    //        break;
+    //    case 'u':
+    //        for (int j = 0; j < cols; ++j) {
+    //            int temp = arr[0][j];
+    //            for (int i = 0; i < rows - 1; ++i) {
+    //                arr[i][j] = arr[i + 1][j];
+    //            }
+    //            arr[rows - 1][j] = temp;
+    //        }
+    //        break;
+    //    case 'd':
+    //        for (int j = 0; j < cols; ++j) {
+    //            int temp = arr[rows - 1][j];
+    //            for (int i = rows - 1; i > 0; --i) {
+    //                arr[i][j] = arr[i - 1][j];
+    //            }
+    //            arr[0][j] = temp;
+    //        }
+    //        break;
+    //    default:
+    //        cout << "Invalid!";
+    //    }
+    //}
+
+    //cout << endl;
+    //for (int i = 0; i < rows; ++i) {
+    //    for (int j = 0; j < cols; ++j) {
+    //        cout << arr[i][j] << " ";
+    //    }
+    //    cout << endl;
+    //}
+
+
+
+
+
+}
