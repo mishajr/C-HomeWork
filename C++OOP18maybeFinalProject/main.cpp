a99c3a3ddc14bed6b3e67347cddee8e339eb2a68
Михайло <153002746+mishajr@users.noreply.github.com>
2024-06-06 23:07:54 +0300

    Add files via upload
    C++OOP18FinalProject

--- C++OOP18maybeFinalProject/main.cpp
+++ C++OOP18maybeFinalProject/main.cpp
@@ -8,29 +8,42 @@
 #include <unordered_set>
 #include <algorithm>
 #include <vector>
-
-
-
-//Я постарався по мінімуму робити пробіли,ще я зроби декілька додаваннь в файл та зчитування для того щоб чимось доповнити роботу))
-
+#include <iomanip>
 
 using namespace std;
 
+bool compare_courses(const pair<string, int> &a, const pair<string, int> &b) {
+    return a.second > b.second;
+}
+
 class CourseManagementSystem {
-
     multimap<string, string> course_students;
-
     map<string, multiset<int>> student_grades;
-
     unordered_set<string> unique_students;
 
-public:
+    vector<string> menu_options = {
+            "Save to file",
+            "Add student to course",
+            "Update student grade",
+            "Remove student from course",
+            "Print all courses and students",
+            "Print unique students",
+            "Print students by grades",
+            "Load from file",
+            "Append to file",
+            "Print file content",
+            "Search student",
+            "Calculate average grade",
+            "Filter students by course",
+            "Print courses sorted by student count",
+            "Exit"
+    };
 
+public:
     void load_from_file(const string &filename) {
         ifstream file(filename);
         if (!file.is_open()) {
             cerr << "Error opening file!" << endl;
-
             return;
         }
 
@@ -38,15 +51,11 @@ public:
         int grade;
         while (file >> course >> student >> grade) {
             add_student_to_course(course, student, grade, false);
-
         }
 
         file.close();
-
-
         print_courses();
     }
-
 
     void add_student_to_course(const string &course, const string &student, int grade, bool writeToFile = true) {
         course_students.emplace(course, student);
@@ -58,7 +67,6 @@ public:
         }
     }
 
-
     void update_student_grade(const string &student, int old_grade, int new_grade) {
         auto &grades = student_grades[student];
         auto it = grades.find(old_grade);
@@ -68,34 +76,27 @@ public:
         }
     }
 
-
     void remove_student_from_course(const string &course, const string &student) {
         auto range = course_students.equal_range(course);
         for (auto it = range.first; it != range.second; ) {
             if (it->second == student) {
                 it = course_students.erase(it);
             } else {
-
                 ++it;
             }
         }
 
-
         if (student_grades[student].empty()) {
             student_grades.erase(student);
-
             unique_students.erase(student);
         }
     }
 
-
     void print_courses() const {
-
         for (const auto &entry : course_students) {
             cout << "Course: " << entry.first << ", Student: " << entry.second << endl;
         }
     }
-
 
     void print_unique_students() const {
         for (const auto &student : unique_students) {
@@ -103,9 +104,7 @@ public:
         }
     }
 
-
     void print_students_by_grades() const {
-
         for (const auto &entry : student_grades) {
             cout << "Student: " << entry.first << ", Grades: ";
             for (const auto &grade : entry.second) {
@@ -115,9 +114,7 @@ public:
         }
     }
 
-
     void save_to_file(const string &filename) const {
-
         ofstream file(filename);
         if (!file.is_open()) {
             cerr << "Error opening file!" << endl;
@@ -125,7 +122,6 @@ public:
         }
 
         for (const auto &entry : course_students) {
-
             const string &course = entry.first;
             const string &student = entry.second;
             for (const auto &grade : student_grades.at(student)) {
@@ -136,9 +132,7 @@ public:
         file.close();
     }
 
-
     void append_to_file(const string &filename, const string &course, const string &student, int grade) const {
-
         ofstream file(filename, ios::app);
         if (!file.is_open()) {
             cerr << "Error opening file!" << endl;
@@ -149,12 +143,10 @@ public:
         file.close();
     }
 
-
     void search_student(const string &student) const {
-
         auto it = student_grades.find(student);
         if (it != student_grades.end()) {
-            cout << "Student: " << student <<endl;
+            cout << "Student: " << student << endl;
             cout << "Courses: ";
             for (const auto &course : course_students) {
                 if (course.second == student) {
@@ -171,23 +163,20 @@ public:
         }
     }
 
-
     void calculate_average_grade(const string &student) const {
-
         auto it = student_grades.find(student);
         if (it != student_grades.end() && !it->second.empty()) {
             double average = accumulate(it->second.begin(), it->second.end(), 0.0) / it->second.size();
-            cout << "Student: " << student <<endl;
+            cout << "Student: " << student << endl;
             cout << "Average Grade: " << average << endl;
         } else {
             cout << "Student not found or no grades available!" << endl;
         }
     }
 
-
     void filter_students_by_course(const string &course) const {
-
-        cout << "Course: " << course << ", Students: ";
+        cout << "Course: " << course <<endl;
+        cout << "Students: " << endl;
         auto range = course_students.equal_range(course);
         for (auto it = range.first; it != range.second; ++it) {
             cout << it->second << " ";
@@ -195,11 +184,6 @@ public:
         cout << endl;
     }
 
-
-    static bool compare_courses(const pair<string, int> &a, const pair<string, int> &b) {
-        return a.second > b.second;
-    }
-//    Тут без статіка я не міг передати це у sort
 
 
     void print_courses_sorted_by_student_count() const {
@@ -217,9 +201,7 @@ public:
         }
     }
 
-
     void print_file_content(const string &filename) const {
-
         ifstream file(filename);
         if (!file.is_open()) {
             cerr << "Error opening file!" << endl;
@@ -235,125 +217,128 @@ public:
     }
 
     void menu() {
-
-        int choice;
+        int current_selection = 0;
+        string input;
         string course, student, filename;
         int grade, old_grade, new_grade;
 
-        do {
+
+
+        auto print_menu = [&]() {
+
             cout << "-------Course Management System Menu--------" << endl;
-            cout << "1. Save to file                             |" << endl;
-            cout << "2. Add student to course                    |" << endl;
-            cout << "3. Update student grade                     |" << endl;
-            cout << "4. Remove student from course               |" << endl;
-            cout << "5. Print all courses and students           |" << endl;
-            cout << "6. Print unique students                    |" << endl;
-            cout << "7. Print students by grades                 |" << endl;
-            cout << "8. Load from file                           |" << endl;
-            cout << "9. Append to file                           |" << endl;
-            cout << "10. Print file content                      |" << endl;
-            cout << "11. Search student                          |" << endl;
-            cout << "12. Calculate average grade                 |" << endl;
-            cout << "13. Filter students by course               |" << endl;
-            cout << "14. Print courses sorted by student count   |" << endl;
-            cout << "15. Exit                                    |" << endl;
-            cout << "--------------------------------------------" << endl;
-            cout << "Enter your choice: " << endl;
-            cin >> choice;
-
-            switch (choice) {
-                case 1:
-                    cout << "Enter filename: " << endl;
-                    cin >> filename;
-                    save_to_file(filename);
-                    break;
-                case 2:
-                    cout << "Enter course name: " << endl;
-                    cin >> course;
-                    cout << "Enter student name: " << endl;
-                    cin >> student;
-                    cout << "Enter grade: " << endl;
-                    cin >> grade;
-                    add_student_to_course(course, student, grade);
-                    break;
-                case 3:
-                    cout << "Enter student name: " << endl;
-                    cin >> student;
-                    cout << "Enter old grade: " << endl;
-                    cin >> old_grade;
-                    cout << "Enter new grade: " << endl;
-                    cin >> new_grade;
-                    update_student_grade(student, old_grade, new_grade);
-                    break;
-                case 4:
-                    cout << "Enter course name: " << endl;
-                    cin >> course;
-                    cout << "Enter student name: " << endl;
-                    cin >> student;
-                    remove_student_from_course(course, student);
-                    break;
-                case 5:
-                    print_courses();
-                    break;
-                case 6:
-                    print_unique_students();
-                    break;
-                case 7:
-                    print_students_by_grades();
-                    break;
-                case 8:
-                    cout << "Enter filename: " << endl;
-                    cin >> filename;
-                    load_from_file(filename);
-                    break;
-                case 9:
-                    cout << "Enter filename: " << endl;
-                    cin >> filename;
-                    cout << "Enter course name: " << endl;
-                    cin >> course;
-                    cout << "Enter student name: " << endl;
-                    cin >> student;
-                    cout << "Enter grade: " << endl;
-                    cin >> grade;
-                    append_to_file(filename, course, student, grade);
-                    break;
-                case 10:
-                    cout << "Enter filename: " << endl;
-                    cin >> filename;
-                    print_file_content(filename);
-                    break;
-                case 11:
-                    cout << "Enter student name: " << endl;
-                    cin >> student;
-                    search_student(student);
-                    break;
-                case 12:
-                    cout << "Enter student name: " << endl;
-                    cin >> student;
-                    calculate_average_grade(student);
-                    break;
-                case 13:
-                    cout << "Enter course name: " <<endl;
-                    cin >> course;
-                    filter_students_by_course(course);
-                    break;
-                case 14:
-                    print_courses_sorted_by_student_count();
-                    break;
-                case 15:
-                    cout << "Exiting..." << endl;
-                    break;
-                default:
-                    cout << "Invalid choice. Please try again." << endl;
+            for (int i = 0; i < menu_options.size(); ++i) {
+                if (i == current_selection) {
+                    cout << "-> ";
+                } else {
+                    cout << "   ";
+                }
+                cout << menu_options[i] << endl;
             }
+            cout << "--------------------------------------------" << endl;
+            cout << "Enter 'n' for next, 'b' for back, 'e' for enter: " << endl;
+        };
 
-        } while (choice != 15);
+        do {
+            print_menu();
+            cin >> input;
+
+            if (input == "n") {
+                current_selection = (current_selection + 1) % menu_options.size();
+            } else if (input == "b") {
+                current_selection = (current_selection - 1 + menu_options.size()) % menu_options.size();
+            } else if (input == "e") {
+                switch (current_selection) {
+                    case 0:
+                        cout << "Enter filename: " << endl;
+                        cin >> filename;
+                        save_to_file(filename);
+                        break;
+                    case 1:
+                        cout << "Enter course name: " << endl;
+                        cin >> course;
+                        cout << "Enter student name: " << endl;
+                        cin >> student;
+                        cout << "Enter grade: " << endl;
+                        cin >> grade;
+                        add_student_to_course(course, student, grade);
+                        break;
+                    case 2:
+                        cout << "Enter student name: " << endl;
+                        cin >> student;
+                        cout << "Enter old grade: " << endl;
+                        cin >> old_grade;
+                        cout << "Enter new grade: " << endl;
+                        cin >> new_grade;
+                        update_student_grade(student, old_grade, new_grade);
+                        break;
+                    case 3:
+                        cout << "Enter course name: " << endl;
+                        cin >> course;
+                        cout << "Enter student name: " << endl;
+                        cin >> student;
+                        remove_student_from_course(course, student);
+                        break;
+                    case 4:
+                        print_courses();
+                        break;
+                    case 5:
+                        print_unique_students();
+                        break;
+                    case 6:
+                        print_students_by_grades();
+                        break;
+                    case 7:
+                        cout << "Enter filename: " << endl;
+                        cin >> filename;
+                        load_from_file(filename);
+                        break;
+                    case 8:
+                        cout << "Enter filename: " << endl;
+                        cin >> filename;
+                        cout << "Enter course name: " << endl;
+                        cin >> course;
+                        cout << "Enter student name: " << endl;
+                        cin >> student;
+                        cout << "Enter grade: " << endl;
+                        cin >> grade;
+                        append_to_file(filename, course, student, grade);
+                        break;
+                    case 9:
+                        cout << "Enter filename: " << endl;
+                        cin >> filename;
+                        print_file_content(filename);
+                        break;
+                    case 10:
+                        cout << "Enter student name: " << endl;
+                        cin >> student;
+                        search_student(student);
+                        break;
+                    case 11:
+                        cout << "Enter student name: " << endl;
+                        cin >> student;
+                        calculate_average_grade(student);
+                        break;
+                    case 12:
+                        cout << "Enter course name: " << endl;
+                        cin >> course;
+                        filter_students_by_course(course);
+                        break;
+                    case 13:
+                        print_courses_sorted_by_student_count();
+                        break;
+                    case 14:
+                        cout << "Exiting..." << endl;
+                        break;
+                    default:
+                        cout << "Invalid choice. Please try again." << endl;
+                }
+            }
+        } while (current_selection != 14 || input != "e");
     }
 };
 
 int main() {
-
     CourseManagementSystem cms;
     cms.menu();
-
 }
